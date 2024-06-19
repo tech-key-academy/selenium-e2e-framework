074bc6bf6a0db740fc0be36eae81b09793b55f1f
pashesqa <159084142+pashesqa@users.noreply.github.com>
2024-06-19 12:15:32 -0600

    Adding blankFieldvalidation

--- src/test/java/FirstTestClass.java
+++ src/test/java/FirstTestClass.java
@@ -1,20 +1,40 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
  public class FirstTestClass {
+     String LANDING_PAGE_URL = "https://magento.softwaretestingboard.com/";
+     String CREATE_AN_ACCOUNT = "//button[@type='submit']";
+
     @Test
     void verifyHomePageLanding()
     {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://magento.softwaretestingboard.com/");
+        driver.get(LANDING_PAGE_URL);
 
         String title = driver.getTitle();
 
         Assert.assertEquals(title,"Home Page");
 
     }
-    
+     @Test
+     void blankFieldvalidation()
+     {
+        WebDriver driver = new ChromeDriver();
+        driver.get(LANDING_PAGE_URL);
+        WebElement createAnAccount = driver.findElement(By.xpath("//a[text()='Create an Account']"));
+        createAnAccount.click();
+        WebElement createAnAccountButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        createAnAccountButton.click();
+        WebElement firstNameValidationMessage = driver.findElement(By.xpath("//div[@for='firstname']"));
+        Assert.assertTrue(firstNameValidationMessage.isDisplayed());
+        WebElement lastNameValidationMessage = driver.findElement(By.xpath("//div[@for='lastname']"));
+        Assert.assertTrue(lastNameValidationMessage.isDisplayed());
+        System.out.println("hello");
+
+     }
 }
 
